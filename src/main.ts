4fb5c363e9ac08b822689f8919a017f9f0217cba
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-01-07 19:44:07 +0900

    openapi定義を自動生成するようにした

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import * as path from 'path'
+import { writeFileSync } from 'fs'
+import { dump } from 'js-yaml'
 
 async function bootstrap() {
   // Nestアプリインスタンスの作成
@@ -16,6 +19,9 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
+
+  const outputPath = path.resolve(process.cwd(), 'openapi.yml')
+  writeFileSync(outputPath, dump(document, {}))
   await app.listen(3000)
 }
 bootstrap()
